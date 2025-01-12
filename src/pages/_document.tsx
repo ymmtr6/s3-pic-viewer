e6bcb194fa515714171d54942a4ca901ee6e77dd
Riku <ymmtr666@gmail.com>
2025-01-13 03:26:53 +0900

    update layout, paging query

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <meta name="description" content="S3 Bucket Image Viewer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { Box, Heading, Spinner, Button, Flex, SimpleGrid } from '@chakra-ui/reac
 import Image from 'next/image';
 import Modal from 'react-modal';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FaXTwitter } from 'react-icons/fa6';
 
 Modal.setAppElement('#__next');
 
@@ -32,6 +35,9 @@ const Home = () => {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentFiles = files.slice(indexOfFirstItem, indexOfLastItem);
 
+
+
+
   const nextPage = () => setCurrentPage((prev) => prev + 1);
   const prevPage = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
 
@@ -47,27 +53,41 @@ const Home = () => {
 
   const imageUrl = (file: any) => `/api/p/${encodeURIComponent(file.Key)}`;
 
-  useEffect(() => {
-    const fetchFiles = async () => {
-      try {
-        const response = await fetch('/api/listFiles');
-        const data = await response.json();
-        if (Array.isArray(data)) {
-          // Sort files by LastModified date in descending order
-          const sortedFiles = data.sort((a, b) => new Date(b.LastModified).getTime() - new Date(a.LastModified).getTime());
-          setFiles(sortedFiles);
-        } else {
-          console.error('Unexpected response format:', data);
-        }
-      } catch (error) {
-        console.error('Error fetching files:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const xUrl = (user: string, id: string) => `https://x.com/${user}/status/${id}`
 
+  const fetchFiles = async () => {
+    try {
+      const response = await fetch('/api/listFiles');
+      const data = await response.json();
+      if (Array.isArray(data)) {
+        // Sort files by LastModified date in descending order
+        const sortedFiles = data.sort((a, b) => new Date(b.LastModified).getTime() - new Date(a.LastModified).getTime());
+        setFiles(sortedFiles);
+      } else {
+        console.error('Unexpected response format:', data);
+      }
+    } catch (error) {
+      console.error('Error fetching files:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page');
+    if (page) {
+      setCurrentPage(Number(page)); // クエリパラメータからページを取得
+    }
     fetchFiles();
   }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // ページトップにスクロール
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', currentPage.toString());
+    window.history.replaceState({}, '', url); // URLを更新
+  }, [currentPage]);
 
   const handleModalClick = (event: React.MouseEvent) => {
     const { clientX, currentTarget } = event;
@@ -84,25 +104,28 @@ const Home = () => {
 
   return (
     <>
+      <Head>
+        <title>S3 Bucket Image Viewer</title>
+      </Head>
       {!modalIsOpen && (
         <Box as="header" position="fixed" top="0" left="0" width="100%" color="white" bg="gray.800" zIndex="1000" p={1} boxShadow="md">
-          <Heading mb={0}>S3 Bucket Image Viewer</Heading>
+          <Heading mb={0} pl={2}>S3 Bucket Image Viewer</Heading>
         </Box>
       )}
       <Box p={5} pt={modalIsOpen ? "0" : "64px"}>
         {loading ? (
           <Spinner />
         ) : (
-          <SimpleGrid columns={[2, null, 4]} gridGap="40px">
+          <SimpleGrid columns={[2, 3, 4]} gridGap="5px">
             {currentFiles.map((file, index) => (
-              <Box key={index} display="flex" justifyContent="center" alignItems="center" overflow="hidden" height="200px" onClick={() => openModal(file)}>
+              <Box key={index} display="flex" justifyContent="center" alignItems="center" overflow="hidden" height="300px" onClick={() => openModal(file)}>
                 {file.Key && (
                   <Image
                     src={imageUrl(file)}
                     alt={file.Key}
-                    width={200}
-                    height={200}
-                    style={{ objectFit: 'contain' }}
+                    width={300}
+                    height={300}
+                    style={{ objectFit: 'contain', width: '100%', height: '100%' }} // 変更点
                     priority={true}
                   />
                 )}
@@ -132,7 +155,7 @@ const Home = () => {
           style={customStyles}
           shouldCloseOnOverlayClick={true}
         >
-          {selectedImage && (
+          {selectedImage && <>
             <Box position="relative" width="100%" height="100%" bg="black" onClick={handleModalClick}>
               <Image
                 src={imageUrl(selectedImage)}
@@ -143,7 +166,14 @@ const Home = () => {
                 quality={100}
               />
             </Box>
-          )}
+            {selectedImage.Key?.split('/').length == 3 &&
+              <Box position="absolute" bottom="20px" right="20px">
+                <Link href={xUrl(selectedImage.Key.split('/')[0], selectedImage.Key.split('/')[1])} target="_blank" rel="noopener noreferrer">
+                  <FaXTwitter />
+                </Link>
+              </Box>
+            }
+          </>}
           <Button onClick={closeModal} position="absolute" top="10px" right="10px">Close</Button>
         </Modal>
       </Box>
