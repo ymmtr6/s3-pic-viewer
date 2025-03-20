d46671863b7ef91e0719490f74921b356f47e816
Riku <ymmtr666@gmail.com>
2025-03-21 05:42:51 +0900

    listObject 1000 over対策

--- src/pages/api/listFiles.ts
+++ src/pages/api/listFiles.ts
@@ -16,9 +16,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const params = {
       Bucket: process.env.AWS_BUCKET_NAME!,
     };
-    const data = await s3.listObjectsV2(params).promise();
-    console.log(data);
-    res.status(200).json(data.Contents);
+    let contents: AWS.S3.ObjectList = []
+    let data = await s3.listObjectsV2(params).promise();
+    while (data.IsTruncated === true) {
+      data = await s3.listObjectsV2({ ...params, ContinuationToken: data.NextContinuationToken }).promise();
+      contents = contents.concat(data.Contents || []);
+    }
+    console.log(contents.length);
+    res.status(200).json(contents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching files from S3', details: error });
   }
